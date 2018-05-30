225c5edfe5a07a062871eb85051091aeb94f5d61
Kenna Rewcastle <kenna.rewcastle@gmail.com>
2018-05-30 11:53:46 -0400

    Code and images for final bog paper figures.

--- current_bog_code.R
+++ current_bog_code.R
@@ -52,32 +52,32 @@ realbog<-read.csv("BOG2015_for_stepAIC.csv")
 
 ##### Model selection using step AIC for resp predictors. These parameters are for the scenario where we include 
 
-# resp<-realbog$D5.CO2
-# MBC<-realbog$MBC.mg.C.g.1.dry.soil
+resp<-realbog$D5.CO2
+MBC<-realbog$MBC.mg.C.g.1.dry.soil
 # logMBC<-log(MBC) # Data is slightly more normally distributed... Not enough to matter!
-# resp_enzyC<-realbog$enzyC
-# resp_enzyNut<-realbog$enzyNut
-# sat<-realbog$per..saturation
-# pH<-realbog$pH
-# DOC<-realbog$DOC..mg.C.g.1.dry.soil.
-# CN<-realbog$peat.C.N
-# spruce<-realbog$Dist..Spruce..m.
-# blub<-realbog$Dist..Blueberry..m.
+resp_enzyC<-realbog$enzyC
+resp_enzyNut<-realbog$enzyNut
+sat<-realbog$per..saturation
+pH<-realbog$pH
+DOC<-realbog$DOC..mg.C.g.1.dry.soil.
+CN<-realbog$peat.C.N
+spruce<-realbog$Dist..Spruce..m.
+blub<-realbog$Dist..Blueberry..m.
 
 # These parameters are on the full dataset (n=59) and excludes MBC and DOC
-resp<-bog$D5.CO2
-resp_enzyC<-bog$enzyC
-resp_enzyNut<-bog$enzyNut
-sat<-bog$per..saturation
-pH<-bog$pH
-CN<-bog$peat.C.N
-spruce<-bog$Dist..Spruce..m.
-blub<-bog$Dist..Blueberry..m.
+# resp<-bog$D5.CO2
+# resp_enzyC<-bog$enzyC
+# resp_enzyNut<-bog$enzyNut
+# sat<-bog$per..saturation
+# pH<-bog$pH
+# CN<-bog$peat.C.N
+# spruce<-bog$Dist..Spruce..m.
+# blub<-bog$Dist..Blueberry..m.
 
 # Figure out parameter order in initial model by comparing resp~paramater R2
 
 # predict1<-data.frame(MBC,resp_enzyC,resp_enzyNut,sat,pH,DOC,CN,spruce,blub)
-predict1<-data.frame(resp_enzyC,resp_enzyNut,sat,pH,CN,spruce,blub)
+predict1<-data.frame(resp_enzyC,resp_enzyNut,sat,pH,CN,spruce,blub,MBC,DOC)
 
 param_R2<-function(predict,response){
   N<-ncol(predict)
@@ -99,10 +99,20 @@ print(resp_rs)
 
 library(MASS)
 
-# mod001<-lm((resp~sat+ resp_enzyNut+ MBC+ spruce+ resp_enzyC+ DOC+ pH+ blub+ CN)
-mod001<-lm(resp~sat+ resp_enzyNut+ spruce+ resp_enzyC+ pH+ CN+ blub)
-stepmod001<-stepAIC(mod001, direction="both") # best model: resp ~ sat + spruce + resp_enzyC + DOC, AIC = 21.63
-summary(stepmod001)
+mod001<-lm(resp~sat+ resp_enzyNut+ MBC+ spruce+ resp_enzyC+ DOC+ pH+ blub+ CN)
+# mod001<-lm(resp~sat+ resp_enzyNut+ spruce+ resp_enzyC+ pH+ CN+ blub)
+startmod<-lm(resp~1)
+stepmod001<-stepAIC(startmod,direction="both",scope=resp~sat+ resp_enzyNut+ MBC+ spruce+ resp_enzyC+ DOC+ pH+ blub+ CN)# best model: resp ~ sat + spruce + resp_enzyC + DOC, AIC = 21.63
+ summary(stepmod001)
+
+mod1<-lm(resp~sat)
+summary(mod1)
+mod2<-lm(resp~sat+resp_enzyC)
+summary(mod2)
+mod3<-lm(resp~sat+resp_enzyC+spruce)
+summary(mod3)
+mod4<-lm(resp~sat+resp_enzyC+spruce+DOC)
+summary(mod4)
 
 ##### Linear regressions between parameters in best fit model and resp
 
@@ -115,6 +125,7 @@ resp_sat_plot<-ggplot(data=realbog,aes(x=sat,y=resp)) +
   ylab(expression(bold(paste("Peat Respiration (",mu,"mol"," ","CO"[2]," ","m"^-2," s"^-1,")")))) +
   xlab(label="Peat Water Saturation (%)") +
   coord_cartesian(xlim=c(40,130), ylim=c(0,8.05)) + # Makes sure that CI and line fit extend to edge of graph
+  annotate("text", x = 40, y = 7.9, label = "A", size=8, color="black") +
   theme(panel.grid.minor=element_blank(),
         panel.grid.major=element_blank(),
         axis.text=element_text(colour="black",size=10),
@@ -132,6 +143,8 @@ resp_spruce_plot<-ggplot(data=realbog,aes(x=spruce,y=resp)) +
   ylab(label=NULL) +
   xlab(label="Distance from Nearest Spruce Tree (m)") +
   ylim(0,8) +
+  xlim(0,2.45) +
+  annotate("text", x = 0, y = 7.9, label = "B", size=8, color="black") +
   theme(panel.grid.minor=element_blank(),
         panel.grid.major=element_blank(),
         axis.text=element_text(colour="black",size=10),
@@ -147,8 +160,9 @@ resp_enzyC_plot<-ggplot(data=realbog,aes(x=resp_enzyC,y=resp)) +
   geom_smooth(method=lm,formula=y~x,colour="black",size=1.25) +
   geom_point() +
   ylab(expression(bold(paste("Peat Respiration (",mu,"mol"," ","CO"[2]," ","m"^-2," s"^-1,")")))) +
-  xlab(expression(bold(paste("Potential C-Degrading Enzyme Activity"," (nmol"," g"^-1," h"^-1,")")))) +
+  xlab(expression(bold(paste("Potential C Enzyme Activity"," (nmol"," g"^-1," h"^-1,")")))) +
   ylim(0,8) +
+  annotate("text", x = 500, y = 7.9, label = "C", size=8, color="black") +
   theme(panel.grid.minor=element_blank(),
         panel.grid.major=element_blank(),
         axis.text=element_text(colour="black",size=10),
@@ -166,6 +180,7 @@ resp_DOC_plot<-ggplot(data=realbog,aes(x=DOC,y=resp)) +
   ylab(label=NULL) +
   xlab(expression(bold(paste("DOC (mg C ","g"^-1," dry soil)")))) +
   ylim(0,8) +
+  annotate("text", x = 1.5, y = 7.9, label = "D", size=8, color="black") +
   theme(panel.grid.minor=element_blank(),
         panel.grid.major=element_blank(),
         axis.text=element_text(colour="black",size=10),
@@ -177,20 +192,21 @@ resp_DOC_plot<-ggplot(data=realbog,aes(x=DOC,y=resp)) +
 ##### Panneled figure for best predictor regressions
 
 library(gridExtra)
-grid.arrange(resp_sat_plot,resp_spruce_plot,resp_enzyC_plot,resp_DOC_plot,nrow=2)
+Figure2<-grid.arrange(resp_sat_plot,resp_spruce_plot,resp_enzyC_plot,resp_DOC_plot,nrow=2)
+ggsave(filename="Figure2.pdf",plot=Figure2,dpi=300,width=8.75,height=7.5,units="in")
 
 ##### Model selection using step AIC for d13 predictors.
 
-# bog_starch<-filter(realbog,carbon=="label")
-bog_starch<-filter(bog,carbon=="label")
+bog_starch<-filter(realbog,carbon=="label")
+# bog_starch<-filter(bog,carbon=="label")
 
 d13<-bog_starch$D5.d13
-# MBC<-bog_starch$MBC.mg.C.g.1.dry.soil
+MBC<-bog_starch$MBC.mg.C.g.1.dry.soil
 starch_enzyC<-bog_starch$enzyC
 starch_enzyNut<-bog_starch$enzyNut
 sat<-bog_starch$per..saturation
 pH<-bog_starch$pH
-# DOC<-bog_starch$DOC..mg.C.g.1.dry.soil.
+DOC<-bog_starch$DOC..mg.C.g.1.dry.soil.
 CN<-bog_starch$peat.C.N
 spruce<-bog_starch$Dist..Spruce..m.
 blub<-bog_starch$Dist..Blueberry..m.
@@ -198,8 +214,8 @@ resp<-bog_starch$D5.CO2
 
 # Figure out parameter order in initial model by comparing resp~paramater R2
 
-# predict2<-data.frame(MBC,starch_enzyC,starch_enzyNut,sat,pH,DOC,CN,spruce,blub)
-predict2<-data.frame(starch_enzyC,starch_enzyNut,sat,pH,CN,spruce,blub)
+predict2<-data.frame(MBC,starch_enzyC,starch_enzyNut,sat,pH,DOC,CN,spruce,blub)
+# predict2<-data.frame(starch_enzyC,starch_enzyNut,sat,pH,CN,spruce,blub)
 
 starch_rs<-param_R2(predict=predict2,response=d13)
 print(starch_rs)
@@ -207,11 +223,19 @@ print(starch_rs)
 # StepAIC model selection
 library(MASS)
 
-# mod002<-lm(d13~spruce+ MBC+ starch_enzyC+ sat+ starch_enzyNut+ DOC+ CN+ blub+ pH)
-mod002<-lm(d13~spruce+ starch_enzyC+ sat+ starch_enzyNut+ CN+ pH+ blub)
-stepmod002<-stepAIC(mod002, direction="both") # best model: resp~ spruce+ starch_enzyC + starch_enzyNut 
-
+mod002<-lm(d13~spruce+ MBC+ starch_enzyC+ sat+ starch_enzyNut+ DOC+ CN+ blub+ pH)
+# mod002<-lm(d13~spruce+ starch_enzyC+ sat+ starch_enzyNut+ CN+ pH+ blub)
+# Best model: resp~ spruce+ starch_enzyC + starch_enzyNut 
+startmod<-lm(d13~1)
+stepmod002<-stepAIC(startmod,direction="both",scope=d13~spruce+ MBC+ starch_enzyC+ sat+ starch_enzyNut+ DOC+ CN+ blub+ pH)
 summary(stepmod002)
+
+mod01<-lm(d13~spruce)
+summary(mod01)
+mod02<-lm(d13~spruce+ starch_enzyC)
+summary(mod02)
+mod03<-lm(d13~spruce + starch_enzyC + starch_enzyNut)
+summary(mod03)
 
 ##### Linear regressions between parameters in best fit model and d13
 
@@ -221,13 +245,16 @@ summary(d13_spruce) # p = 0.002, R2 = 0.35
 d13_spruce_plot<-ggplot(data=bog_starch,aes(x=spruce,y=d13)) +
   geom_smooth(method=lm,formula=y~x,fullrange=TRUE,colour="black",size=1.25) +
   geom_point() +
-  ylab(label=expression(bold(paste("\u03B4"^{bold("13")}, "C (\u2030)")))) +
-  xlab(label="Distance from Nearest Spruce Tree (cm)") +
+  ylab(label=expression(bold(paste("\u03B4"^{bold("13")}, "C Respired (\u2030)")))) +
+  xlab(label="Distance from Nearest Spruce Tree (m)") +
   ylim(0,400) +
+  xlim(0,2.45) +
+  annotate("text", x = 0, y = 400, label = "A", size=8, color="black") +
   theme(panel.grid.minor=element_blank(),
         panel.grid.major=element_blank(),
         axis.text=element_text(colour="black",size=10),
-        axis.title=element_text(size=14,face="bold"),
+        axis.title.y=element_text(size=14,face="bold"),
+        axis.title.x=element_text(size=12,face="bold"),
         panel.border=element_rect(fill=NA,colour="black",size=1.5),
         panel.background=element_rect(fill=NA)
   )
@@ -239,12 +266,14 @@ d13_enzyC_plot<-ggplot(data=bog_starch,aes(x=starch_enzyC,y=d13)) +
   geom_smooth(method=lm,formula=y~x,colour="black",size=1.25) +
   geom_point() +
   ylab(label=NULL) +
-  xlab(expression(bold(paste("Potential C-Degrading Enzyme Activity"," (nmol"," g"^-1," h"^-1,")")))) +
+  xlab(expression(bold(paste("Potential C Enzyme Activity"," (nmol"," g"^-1," h"^-1,")")))) +
   ylim(0,400) +
+  annotate("text", x = 600, y = 400, label = "B", size=8, color="black") +
   theme(panel.grid.minor=element_blank(),
         panel.grid.major=element_blank(),
         axis.text=element_text(colour="black",size=10),
-        axis.title=element_text(size=14,face="bold"),
+        axis.title.y=element_text(size=14,face="bold"),
+        axis.title.x=element_text(size=12,face="bold"),
         panel.border=element_rect(fill=NA,colour="black",size=1.5),
         panel.background=element_rect(fill=NA)
   )
@@ -258,10 +287,12 @@ d13_enzyNut_plot<-ggplot(data=bog_starch,aes(x=starch_enzyNut,y=d13)) +
   ylab(label=NULL) +
   xlab(expression(bold(paste("Potential Nutrient Enzyme Activity"," (nmol"," g"^-1," h"^-1,")")))) +
   ylim(0,400) +
+  annotate("text", x = 400, y = 400, label = "C", size=8, color="black") +
   theme(panel.grid.minor=element_blank(),
         panel.grid.major=element_blank(),
         axis.text=element_text(colour="black",size=10),
-        axis.title=element_text(size=14,face="bold"),
+        axis.title.y=element_text(size=14,face="bold"),
+        axis.title.x=element_text(size=12,face="bold"),
         panel.border=element_rect(fill=NA,colour="black",size=1.5),
         panel.background=element_rect(fill=NA)
   )
@@ -269,7 +300,8 @@ d13_enzyNut_plot<-ggplot(data=bog_starch,aes(x=starch_enzyNut,y=d13)) +
 ##### Panneled figure for best predictor regressions
 
 library(gridExtra)
-grid.arrange(d13_spruce_plot,d13_enzyC_plot,d13_enzyNut_plot,nrow=1)
+Figure1<-grid.arrange(d13_spruce_plot,d13_enzyC_plot,d13_enzyNut_plot,nrow=1)
+# ggsave(filename="Figure1.jpeg",plot=Figure1,dpi=300,width=11,units="in")
 
 ###### Does respiration predict d13?
 starch_resp<-lm(d13~resp)
@@ -347,20 +379,20 @@ write.csv(tableOutput,file="table_means.csv") # Creates a .csv file in working d
 
 ##### Mesh size does not affect microbial biomass (plot and stats)
 
-meshMBC<-as.factor(bogMBC$treatment)
-MBC<-bogMBC$MBC.mg.C.g.1.dry.soil
+mesh<-as.factor(bog$treatment)
+MBC<-bog$MBC.mg.C.g.1.dry.soil
 
-mesh_MBC<-lm(MBC~meshMBC)
-anova(mesh_MBC) # p = 0.09981
+mesh_MBC<-lm(MBC~mesh)
+anova(mesh_MBC) # p = 0.204, F = 1.646
 summary(mesh_MBC)
 
 mesh_cols<-c("1"="grey60","2"="grey32","3"="black")
 mesh_scale<-c(1,2,3)
 
-mesh_MBC_fig<-ggplot(data=bogMBC,aes(x=meshMBC,y=MBC,colour=meshMBC)) +
+mesh_MBC_fig<-ggplot(data=bog,aes(x=mesh,y=MBC)) +
   geom_boxplot(lwd=1.5) +
   labs(x="Mesh Size",y=bquote(bold('Microbial Biomass C (mg C '*g^-1*' dry soil)'))) +
-  scale_colour_manual(values=mesh_cols,labels=c("1.45 mm","55 \u03BCm","5 \u03BCm"),guide=FALSE) + 
+  # scale_colour_manual(values=mesh_cols,labels=c("1.45 mm","55 \u03BCm","5 \u03BCm"),guide=FALSE) + 
   # weird text is unicode for mu
   annotate("text",x=3.4,y=4.1,label="A",size=10) +
   scale_x_discrete(limits=mesh_scale, labels=c("1.45 mm","55 \u03BCm","5 \u03BCm")) +
@@ -378,16 +410,16 @@ mesh<-as.factor(bog$treatment)
 sat<-bog$per..saturation
 
 mesh_sat<-lm(sat~mesh)
-anova(mesh_sat) # p = 0.09372
+anova(mesh_sat) # p = 0.09372, F = 2.470
 summary(mesh_sat)
 
 mesh_cols<-c("1"="grey60","2"="grey32","3"="black")
 mesh_scale<-c(1,2,3)
 
-mesh_sat_fig<-ggplot(data=bog,aes(x=mesh,y=sat,colour=mesh)) +
+mesh_sat_fig<-ggplot(data=bog,aes(x=mesh,y=sat)) +
   geom_boxplot(lwd=1.5) +
   labs(x="Mesh Size",y="% Peat Water Saturation") +
-  scale_colour_manual(values=mesh_cols,labels=c("1.45 mm","55 \u03BCm","5 \u03BCm"),guide=FALSE) + 
+  # scale_colour_manual(values=mesh_cols,labels=c("1.45 mm","55 \u03BCm","5 \u03BCm"),guide=FALSE) + 
   # weird text is unicode for mu
   annotate("text",x=3.4,y=128,label="B",size=10) +
   scale_x_discrete(limits=mesh_scale, labels=c("1.45 mm","55 \u03BCm","5 \u03BCm")) +
@@ -402,27 +434,28 @@ mesh_sat_fig
 ##### Paneled figure for supplemental Figure S1
 
 library(gridExtra)
-grid.arrange(mesh_MBC_fig,mesh_sat_fig,nrow=1)
+S1<-grid.arrange(mesh_MBC_fig,mesh_sat_fig,nrow=1)
+ggsave(filename="S1.jpeg",plot=S1,dpi=300)
 
 ##### Labeled starch treatment was successful (increase in 13C CO2 respiration in labeled mesocosms)
 
-carbon<-as.factor(bog1$carbon)
-d13<-bog1$D5.d13
+carbon<-as.factor(bog$carbon)
+d13<-bog$D5.d13
 
 d13effect<-t.test(formula=d13~carbon) # p < 0.001, t = -9.5717
-labeled<-filter(bog1,carbon=="label")
-water<-filter(bog1,carbon=="control")
-mean(labeled$D5.d13) # 136.05
-sd(labeled$D5.d13) # 82.06
+labeled<-filter(bog,carbon=="label")
+water<-filter(bog,carbon=="control")
+mean(labeled$D5.d13) # 134.38
+sd(labeled$D5.d13) # 82.99
 mean(water$D5.d13) # -7.82
 sd(water$D5.d13) # 6.65
 
 # Figure for S2
 d13_scale<-c(1,2)
 
-d13effect_fig<-ggplot(data=bog1,aes(x=carbon,y=d13)) +
+d13effect_fig<-ggplot(data=bog,aes(x=carbon,y=d13)) +
   geom_boxplot(lwd=1.5) +
-  labs(x=expression(bold(paste(text=" "^{bold("13")},"C"," Starch Addition"))),y=expression(bold(paste("\u03B4"^{bold("13")}, "C (\u2030)")))) +
+  labs(x=expression(bold(paste(text=" "^{bold("13")},"C"," Starch Addition"))),y=expression(bold(paste("\u03B4"^{bold("13")}, "C Respired (\u2030)")))) +
   scale_x_discrete(labels=c("control"="Control","label"="Labeled")) +
   theme(panel.grid.minor=element_blank(),panel.grid.major=element_blank(),
         axis.text.y=element_text(colour="black",size=10),
@@ -431,10 +464,11 @@ d13effect_fig<-ggplot(data=bog1,aes(x=carbon,y=d13)) +
         panel.border=element_rect(fill=NA,colour="black",size=1.5),
         panel.background=element_rect(fill=NA))
 
+## Save plot
+ggsave(filename="S2.jpeg",plot=d13effect_fig,dpi=300)
+
 
 #### Starch addition treatment doesn't affect: total resp, microbial biomass, enzyme activity (all 6), peat saturation, pH
-
-bog<-read.csv("Master_BOG2015_forR.csv")
 
 carbon<-as.factor(bog$carbon)
 resp<-bog$D5.CO2
@@ -449,6 +483,8 @@ sat<-bog$per..saturation
 pH<-bog$pH
 DOC<-bog$DOC..mg.C.g.1.dry.soil.
 CN<-bog$peat.C.N
+enzyC<-bog$enzyC
+enzyNut<-bog$enzyNut
 
 # t-tests first
 t.test(formula=resp~carbon) # p = 0.6186, t = -0.50054, RESPIRATION
@@ -459,6 +495,8 @@ starch_CBH<-t.test(formula=CBH~carbon) # p = 0.1607, t = -1.4225
 starch_NAG<-t.test(formula=NAG~carbon) # p = 0.3001, t = 1.0463
 starch_PHOS<-t.test(formula=PHOS~carbon) # p = 0.166, t = -1.4057
 starch_LAP<-t.test(formula=LAP~carbon) # p = 0.6616, t = -0.44
+starch_enzyC<-t.test(formula=enzyC~carbon)
+starch_enzyNut<-t.test(formula=enzyNut~carbon)
 starch_sat<-t.test(formula=sat~carbon) # p = 0.2097, t = -1.2694 
 starch_pH<-t.test(formula=pH~carbon) # p = 0.8739, t = -0.15945
 starch_DOC<-t.test(formula=DOC~carbon) # p = 0.2034, t = 1.2964
@@ -466,3 +504,16 @@ starch_CN<-t.test(formula=CN~carbon) # p = 0.9167, t = -0.10508
 
 # MIGHT need to build a supplemental figure displaying all of this
 
+##### Respiration and d13 by DOC and MBC to validate removing from model selection
+
+DOC_resp<-lm(resp~DOC)
+summary(DOC_resp)
+
+MBC_resp<-lm(resp~MBC)
+summary(MBC_resp)
+
+DOC_d13<-lm(d13~DOC)
+summary(DOC_d13)
+
+MBC_d13<-lm(d13~MBC)
+summary(MBC_d13)
